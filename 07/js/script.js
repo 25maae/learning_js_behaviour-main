0142e23d25451e2384cab24872a332d1236a0390
25maae <eaa25maae@students.eaaa.dk>
2025-10-23 17:53:39 +0200

    Slogans

--- 07/js/script.js
+++ 07/js/script.js
@@ -18,12 +18,22 @@
 //      }
 // });
 
-const getWordElem = document.getElementById("word");
+const getWordElemNike = document.getElementById("word-nike");
 
-getWordElem.addEventListener("click", function () {
+getWordElemNike.addEventListener("click", function () {
   if (this.textContent == "Just Do It") {
     this.innerHTML = "<strong>Nike</strong>";
   } else {
     this.innerHTML = "Just Do It";
+  }
+});
+
+const getWordElemBilka = document.getElementById("word-bilka");
+
+getWordElemBilka.addEventListener("click", function () {
+  if (this.textContent == "Hvem ka") {
+    this.innerHTML = "<strong>BILKA!</strong>";
+  } else {
+    this.innerHTML = "Hvem ka";
   }
 });
--- 08/js/script.js
+++ 08/js/script.js
@@ -1,34 +1,9 @@
 "use strict";
 
-
-// const getWordElem = document.getElementById("word");
-
-// getWordElem.addEventListener("click", function() {
-//     this.classList.add("bounce");
-//     setTimeout(()=>this.classList.remove("bounce"),600);
-// });
-
-
-// const getWordElem = document.getElementById("word");
-
-// getWordElem.addEventListener("click", function() {
-//     this.classList.add("shake");
-//     setTimeout(()=>this.classList.remove("shake"),400);
-// });
-
-
-// const getWordElem = document.getElementById("word");
-
-// getWordElem.addEventListener("click", function() {
-//     this.classList.add("spin");
-//     setTimeout(()=>this.classList.remove("spin"),600);
-// });
-
 const getWordElem = document.getElementById("word");
 
-getWordElem.addEventListener("click", function() {
-    this.classList.add("fade");
-    setTimeout(()=>this.classList.remove("fade"),1000);
+getWordElem.addEventListener("click", function () {
+  setTimeout(() => this.classList.remove("fade"), 1000);
 });
 
-
+getWordElem.classList.add("myAnimation");
