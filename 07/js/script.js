62f8b4e5b3fe898f4cba82ff8f1a017db2dd7908
25maae <eaa25maae@students.eaaa.dk>
2025-10-23 19:54:37 +0200

    Prettier

--- 07/js/script.js
+++ 07/js/script.js
@@ -21,19 +21,19 @@
 const getWordElemNike = document.getElementById("word-nike");
 
 getWordElemNike.addEventListener("click", function () {
-  if (this.textContent == "Just Do It") {
+  if (this.textContent == "Just Do It!") {
     this.innerHTML = "<strong>Nike</strong>";
   } else {
-    this.innerHTML = "Just Do It";
+    this.innerHTML = "Just Do It!";
   }
 });
 
 const getWordElemBilka = document.getElementById("word-bilka");
 
 getWordElemBilka.addEventListener("click", function () {
-  if (this.textContent == "Hvem ka") {
+  if (this.textContent == "Hvem ka?") {
     this.innerHTML = "<strong>BILKA!</strong>";
   } else {
-    this.innerHTML = "Hvem ka";
+    this.innerHTML = "Hvem ka?";
   }
 });
