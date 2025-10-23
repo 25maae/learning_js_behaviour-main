df2ab06cef7f3879b99f610fe399971ce91f9edb
25maae <eaa25maae@students.eaaa.dk>
2025-10-23 10:26:21 +0200

    Update script.js

--- 00-jsbasics/js/script.js
+++ 00-jsbasics/js/script.js
@@ -8,20 +8,18 @@ console.log("Hello World!"); // Viser tekst i browserens konsol //
 // Variabler //
 const pi = 3.14; // Konstant variabel, kan ikke ændres //
 let age = 21; // Variabel, kan ændres //
-let FirstName = "Simon"; // String variabel //
-let firstName = "Mathilde";
+let FirstName = "Simon"; // PascalCase variabel (Navngivningskonvention) //
+let firstName = "Mathilde"; // CamelCase variabel (Navngivningskonvention) //
+let first_name = "Martin"; // Snake_case variabel (Navngivningskonvention) //
 let SimonAge = 21; // Numerisk variabel //
 let mathildeAge = 99;
+let martin_age = 23;
 
 console.log(`jeg hedder ${FirstName} og jeg er ${SimonAge} år`); // Jeg hedder Simon og jeg er 21 år
 console.log(`jeg hedder ${firstName} og jeg er ${mathildeAge} år`); // Jeg hedder Mathilde og jeg er 99 år
+console.log(`jeg hedder ${first_name} og jeg er ${martin_age} år`); // Jeg hedder Martin og jeg er 23 år
 
-// Navngivningskonventioner //
-// let firstName = "John"; // (CamelCase) //
-// let FirstName = "Jane"; // (PascalCase) //
-// let first_name = "Doe"; // (snake_case) //
-
-// Ændring af variabler //
+// Aritmetiske operationer //
 age = 22; // Ændrer værdien af variablen age //
 age += 1; // Øger værdien af age med 1 //
 age -= 1; // Mindsker værdien af age med 1 //
