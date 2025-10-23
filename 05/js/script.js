3b32a340717cbfbb809955ead2663279e3386e36
25maae <eaa25maae@students.eaaa.dk>
2025-10-23 16:47:03 +0200

    Beer

--- 05/js/script.js
+++ 05/js/script.js
@@ -1,19 +1,14 @@
 "use strict";
 
-
 const changeImgBtn = document.getElementById("changeImage");
 const getImage = document.getElementById("imagePlaceholder");
 
-
-
-changeImgBtn.addEventListener("click", function() {
-
-    if (this.textContent == "Before"){
-        getImage.src= "img/before.png";
-        this.textContent = "After";
-    }
-    else {
-        getImage.src= "img/after.png";
-        this.textContent = "Before";
-     }
+changeImgBtn.addEventListener("click", function () {
+  if (this.textContent == "Fill beer") {
+    getImage.src = "img/full.png";
+    this.textContent = "Drink beer";
+  } else {
+    getImage.src = "img/empty.png";
+    this.textContent = "Fill beer";
+  }
 });
