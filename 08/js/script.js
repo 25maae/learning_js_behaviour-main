5f15a25a364ff5ba8be9e15e01535a7d283a57e4
25maae <eaa25maae@students.eaaa.dk>
2025-10-23 19:17:26 +0200

    Update script.js

--- 08/js/script.js
+++ 08/js/script.js
@@ -2,25 +2,6 @@
 
 // const getWordElem = document.getElementById("word");
 
-// getWordElem.addEventListener("click", function() {
-//     this.classList.add("bounce");
-//     setTimeout(()=>this.classList.remove("bounce"),600);
-// });
-
-// const getWordElem = document.getElementById("word");
-
-// getWordElem.addEventListener("click", function() {
-//     this.classList.add("shake");
-//     setTimeout(()=>this.classList.remove("shake"),400);
-// });
-
-// const getWordElem = document.getElementById("word");
-
-// getWordElem.addEventListener("click", function() {
-//     this.classList.add("spin");
-//     setTimeout(()=>this.classList.remove("spin"),600);
-// });
-
 const getWordElem = document.getElementById("word");
 
 // Fjerner fade animationen uden at fjerne den fra css //
@@ -28,7 +9,12 @@ getWordElem.addEventListener("click", function () {
   this.classList.remove("fade");
 });
 
-// Tilføjer spin animationen //
-getWordElem.addEventListener("click", function() {
-this.classList.add("spin");
-}); 
+// Tilføjer spin animationen ved klik på "Hop!" //
+getWordElem.addEventListener("click", function () {
+  this.classList.add("spin");
+});
+
+// Tilføjer shake animationen efter timeout tiden //
+setTimeout(() => {
+  getWordElem.classList.add("shake");
+}, 5000);
