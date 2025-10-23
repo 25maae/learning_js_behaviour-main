a8b3914faddf009f7e149a7519a6d1d2a3c62714
25maae <eaa25maae@students.eaaa.dk>
2025-10-23 19:03:55 +0200

    Animationer

--- 08/js/script.js
+++ 08/js/script.js
@@ -1,9 +1,34 @@
 "use strict";
 
+// const getWordElem = document.getElementById("word");
+
+// getWordElem.addEventListener("click", function() {
+//     this.classList.add("bounce");
+//     setTimeout(()=>this.classList.remove("bounce"),600);
+// });
+
+// const getWordElem = document.getElementById("word");
+
+// getWordElem.addEventListener("click", function() {
+//     this.classList.add("shake");
+//     setTimeout(()=>this.classList.remove("shake"),400);
+// });
+
+// const getWordElem = document.getElementById("word");
+
+// getWordElem.addEventListener("click", function() {
+//     this.classList.add("spin");
+//     setTimeout(()=>this.classList.remove("spin"),600);
+// });
+
 const getWordElem = document.getElementById("word");
 
+// Fjerner fade animationen uden at fjerne den fra css //
 getWordElem.addEventListener("click", function () {
-  setTimeout(() => this.classList.remove("fade"), 1000);
+  this.classList.remove("fade");
 });
 
-getWordElem.classList.add("myAnimation");
+// Tilføjer spin animationen //
+getWordElem.addEventListener("click", function() {
+this.classList.add("spin");
+}); 
